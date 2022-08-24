befc02d72a313e99432656fb512ab2e145f2fb2d
Erik Yuzwa <erikyuzwa@gmail.com>
2022-08-24 15:17:26 -0600

    cleaning up sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,4 +4,5 @@ INSERT INTO users (username, password, enabled) VALUES ('admin', 'password', tru
 
 -- create the authorities
 INSERT INTO authorities (username, authority) VALUES ('user', 'ROLE_USER');
+INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_USER');
 INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_ADMIN');
